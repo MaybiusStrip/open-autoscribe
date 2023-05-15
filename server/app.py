6865fd61fdfab3d9598c8507c83561d1962570c3
Martin Wintz <martin.wintz@gmail.com>
2023-05-15 13:51:34 -0400

    Added a couple routes

--- server/app.py
+++ server/app.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'Index Page'
+
 @app.route('/start-recording', methods=['POST'])
 def start_recording():
     return 'Start recording endpoint'
@@ -10,6 +14,11 @@ def start_recording():
 @app.route('/stop-recording', methods=['POST'])
 def stop_recording():
     return 'Stop recording endpoint'
+
+@app.errorhandler(404)
+def not_found(e):
+    return 'Not found', 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
